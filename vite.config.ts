15d7386086ba128faa183ec73a5b494d15b1249a
Viktor Sonin <soninviktorforjob@gmail.com>
2025-05-07 04:02:21 +0300

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/redux-toolkit-todo/",
+	base: "/redux-toolkit-todos/",
 	build: { outDir: "build" },
 	plugins: [react()],
 	resolve: {
